494bacfcf2fc5d6751abacb0bf8657696e03de79
b-biswas <bbiswas.dgp@gmail.com>
2024-03-28 05:00:26 +0100

    add function

--- blendxpz/simulations/ssi.py
+++ blendxpz/simulations/ssi.py
@@ -1,11 +1,14 @@
 """SSI in a stamp"""
 
 
-def ssi(btk_draw_generator, isolated_galaxy):
-    """Inject sources into the stamp.
+def ssi_on_simulations(btk_draw_generator, isolated_galaxy):
+    """Inject sources into a simulated stamp.
 
     Parameters
     """
     blend = next(btk_draw_generator)
     source_injected = blend.isolated_images[:, 0] + isolated_galaxy
     return source_injected, blend
+
+def ssi_on_realdata():
+    pass
