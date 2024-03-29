b585a35adb90a1837cc5591bc97447696ea1d9c6
b-biswas <bbiswas.dgp@gmail.com>
2024-03-29 04:36:00 +0100

    adding config

--- blendxpz/utils.py
+++ blendxpz/utils.py
@@ -14,3 +14,18 @@ def get_data_dir_path():
     data_dir = os.path.join(curdir, "data")
 
     return data_dir
+
+
+def get_blendxpz_config_path():
+    """Fetch path to madness_deblender config yaml file.
+
+    Returns
+    -------
+    data_dir: str
+        path to data folder
+
+    """
+    curdir = os.path.dirname(os.path.abspath(__file__))
+    blendxpz_config_path = os.path.join(curdir, "blendxpz_config.yaml")
+
+    return blendxpz_config_path
