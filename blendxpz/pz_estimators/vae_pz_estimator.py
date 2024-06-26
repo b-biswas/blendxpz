1986c9a0908ab1c4bc60953fb88fbfcb3fea8c15
b-biswas <bbiswas.dgp@gmail.com>
2024-06-26 09:43:57 +0200

    moved models

--- blendxpz/pz_estimators/vae_pz_estimator.py
+++ blendxpz/pz_estimators/vae_pz_estimator.py
@@ -2,15 +2,30 @@ import logging
 
 import tensorflow as tf
 import tensorflow_probability as tfp
-from tensorflow.keras.layers import Input
+from tensorflow.keras.layers import Input, Dense
 from tensorflow.keras.models import Model
-
-from blendxpz.model import create_pz_estimator
 
 tfd = tfp.distributions
 
 LOG = logging.getLogger(__name__)
 
+def create_pz_estimator(latent_dim):
+
+    input_layer = Input(shape=(latent_dim))
+    h = Dense(32, "tanh")(input_layer)
+    # h = PReLU()(h)
+    h = Dense(64, "tanh")(h)
+    # h = PReLU()(h)
+    h = Dense(128, "tanh")(h)
+    # h = PReLU()(h)
+    h = tf.keras.layers.BatchNormalization()(h)
+    h = Dense(64, "tanh")(h)
+    # h = PReLU()(h)
+    h = Dense(32, "tanh")(h)
+    # h = PReLU()(h)
+    mu = Dense(1, activation="relu")(h)
+    # sig = Dense(1, activation="relu")(h) + 0.01
+    return Model(input_layer, mu, name="pz-estimator")
 
 def create_vae_pz_model(encoder, decoder, input_shape, latent_dim):
     encoder.trainable = False
@@ -25,6 +40,14 @@ def create_vae_pz_model(encoder, decoder, input_shape, latent_dim):
     pz_estimator = create_pz_estimator(latent_dim=latent_dim)
 
     return Model(inputs=x_input, outputs=(decoder(z), pz_estimator(z))), pz_estimator
+
+def create_photometry_pz_model(num_bands):
+
+    # Link the models
+    x_input = Input(shape=(num_bands))
+    pz_estimator = create_pz_estimator(latent_dim=5)
+
+    return pz_estimator
 
 
 def train_pz(
@@ -100,3 +123,71 @@ def train_pz(
         use_multiprocessing=True,
     )
     return hist
+
+
+# def train_CNN_pz(
+#     input_shape,
+#     train_generator,
+#     validation_generator,
+#     callbacks,
+#     loss_function,
+#     optimizer=tf.keras.optimizers.Adam(1e-4),
+#     epochs=100,
+#     verbose=1,
+# ):
+#     """Train only the the components of VAE model (encoder and/or decoder).
+
+#     Parameters
+#     ----------
+#     train_generator:
+#         generator to be used for training the network.
+#         keras.utils.Sequence returning (inputs, targets) or (inputs, targets, sample_weights)
+#     validation_generator:
+#         generator to be used for validation
+#         keras.utils.Sequence returning (inputs, targets) or (inputs, targets, sample_weights)
+#     callbacks: list
+#         List of keras.callbacks.Callback instances.
+#         List of callbacks to apply during training.
+#         See tf.keras.callbacks
+#     loss_function: python function
+#         function that can compute the loss.
+#     optimizer: str or tf.keras.optimizers
+#         String (name of optimizer) or optimizer instance. See tf.keras.optimizers.
+#     epochs: int
+#         number of epochs for which the model is going to be trained
+#     verbose: int
+#         verbose option for training.
+#         'auto', 0, 1, or 2. Verbosity mode. 0 = silent, 1 = progress bar, 2 = one line per epoch.
+#         'auto' defaults to 1 for most cases, but 2 when used with ParameterServerStrategy.
+#         Note that the progress bar is not particularly useful when logged to a file, so verbose=2 is recommended when not running interactively (eg, in a production environment).
+
+#     """
+
+#     LOG.info("Number of epochs: " + str(epochs))
+
+#     pz_model = image_CNN_estimator(
+#         encoder, decoder, input_shape, latent_dim
+#     )
+
+#     if loss_function is None:
+#         print("pass valid loss function")
+
+#     pz_model.compile(
+#         optimizer=optimizer,
+#         loss=loss_function,
+#         experimental_run_tf_function=False,
+#     )
+#     print(pz_model.summary())
+
+#     hist = pz_model.fit(
+#         x=train_generator[0] if isinstance(train_generator, tuple) else train_generator,
+#         y=train_generator[1] if isinstance(train_generator, tuple) else None,
+#         epochs=epochs,
+#         verbose=verbose,
+#         shuffle=True,
+#         validation_data=validation_generator,
+#         callbacks=callbacks,
+#         workers=8,
+#         use_multiprocessing=True,
+#     )
+#     return hist
