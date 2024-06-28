c19068dbb3ada9f94ae13acba0663e4f60e3c473
Biswajit Biswas <44917825+b-biswas@users.noreply.github.com>
2024-06-28 10:00:42 +0200

    simulate test fields (madness) (#17)
    * code to test madness data
    
    * test data config

---
+++ scripts/simulations/madness_testdata.py
@@ -0,0 +1,91 @@
+"""Simulate test dataset."""
+
+import logging
+import os
+import pickle
+import sys
+
+import btk
+import yaml
+from astropy.table import Table
+
+from blendxpz.simulations.sampling import CustomSampling
+from blendxpz.utils import get_blendxpz_config_path, get_madness_config_path
+
+# logging level set to INFO
+logging.basicConfig(format="%(message)s", level=logging.INFO)
+
+LOG = logging.getLogger(__name__)
+
+density = sys.argv[1]
+
+if density not in ["high", "low"]:
+    raise ValueError("The first argument should be either high or low")
+
+with open(get_blendxpz_config_path()) as f:
+    blendxpz_config = yaml.safe_load(f)
+
+with open(get_madness_config_path()) as f:
+    madness_config = yaml.safe_load(f)
+
+survey_name = blendxpz_config["SURVEY_NAME"]
+btksims_config = madness_config["btksims"]
+
+survey = btk.survey.get_surveys(survey_name)
+simulation_path = btksims_config["TEST_DATA_SAVE_PATH"][survey_name]
+CATALOG_PATH = btksims_config["CAT_PATH"][survey_name]
+
+print(CATALOG_PATH)
+
+sim_config = btksims_config["TEST_PARAMS"]
+
+if type(CATALOG_PATH) == list:
+    catalog = btk.catalog.CosmosCatalog.from_file(CATALOG_PATH, exclusion_level="none")
+    generator = btk.draw_blends.CosmosGenerator
+else:
+    catalog = btk.catalog.CatsimCatalog.from_file(CATALOG_PATH)
+    generator = btk.draw_blends.CatsimGenerator
+
+# Shuffle to make sure distributions are consistent across train, test, and validation sets.
+# Be careful with the random state, it should be the same in the validation set.
+catalog.table = Table.from_pandas(
+    catalog.table.to_pandas().sample(frac=1, random_state=0).reset_index(drop=True)
+)
+print(sim_config)
+index_range = [sim_config[survey_name]["index_start"], len(catalog.table)]
+sampling_function = CustomSampling(
+    index_range=index_range,
+    min_number=sim_config[density + "_density"]["min_number"],
+    max_number=sim_config[density + "_density"]["max_number"],
+    maxshift=sim_config["maxshift"],
+    stamp_size=sim_config["stamp_size"],
+    seed=sim_config["btk_seed"],
+    unique=sim_config["unique_galaxies"],
+    dataset="test",
+    pixel_scale=0.2,
+)
+
+draw_generator = generator(
+    catalog,
+    sampling_function,
+    survey,
+    batch_size=sim_config["btk_batch_size"],
+    stamp_size=sim_config["stamp_size"],
+    njobs=16,
+    add_noise="all",
+    verbose=False,
+    seed=sim_config["btk_seed"],
+)
+
+for file_num in range(sim_config[survey_name]["num_files"]):
+    print("Processing file " + str(file_num))
+    blend = next(draw_generator)
+
+    save_file_name = os.path.join(
+        simulation_path,
+        density,
+        str(file_num) + ".pkl",
+    )
+    print(save_file_name)
+    with open(save_file_name, "wb") as pickle_file:
+        pickle.dump(blend, pickle_file)
