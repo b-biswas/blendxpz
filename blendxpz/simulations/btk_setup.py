c62c1bfd73ec39a4eef31b0123e3b51076f73f04
b-biswas <bbiswas.dgp@gmail.com>
2024-07-01 14:48:17 +0200

    add documentation

--- blendxpz/simulations/btk_setup.py
+++ blendxpz/simulations/btk_setup.py
@@ -15,24 +15,49 @@ def HSC_filter_snr_adjust(survey):
     return survey
 
 
-def btk_setup_helper(survey_name, btksims_config):
+def btk_setup_helper(survey_name, btksims_config=None):
+    """Create catalog, generatorm and survey objects for btk.
+
+    Parameters
+    ----------
+    survery_name: string
+        name of the survey.
+    btksims_config: dict
+        config file for btk_sims
+
+    Returns
+    -------
+    catalog: astropy.table.Table
+        either an object of btk.catalog.CatsimCatalog (LSST)
+        or an object of btk.catalog.CosmosCatalog (HSC)
+    generator: btk.draw_blends.Generator
+        either an object of btk.draw_blends.CatsimGenerator
+        or btk.draw_blends.CosmosGenerator
+    survey: btk.survey.Survey
+        the survey object
+    """
     survey = btk.survey.get_surveys(survey_name)
     if survey_name == "HSC":
         survey = HSC_filter_snr_adjust(survey)
 
-    CATALOG_PATH = btksims_config["CAT_PATH"][survey_name]
+    catalog = None
+    generator = None
 
-    if type(CATALOG_PATH) == list:
-        catalog = btk.catalog.CosmosCatalog.from_file(
-            CATALOG_PATH, exclusion_level="none"
+    if btksims_config is not None:
+
+        CATALOG_PATH = btksims_config["CAT_PATH"][survey_name]
+
+        if type(CATALOG_PATH) == list:
+            catalog = btk.catalog.CosmosCatalog.from_file(
+                CATALOG_PATH, exclusion_level="none"
+            )
+            generator = btk.draw_blends.CosmosGenerator
+        else:
+            catalog = btk.catalog.CatsimCatalog.from_file(CATALOG_PATH)
+            generator = btk.draw_blends.CatsimGenerator
+
+        catalog.table = Table.from_pandas(
+            catalog.table.to_pandas().sample(frac=1, random_state=0).reset_index(drop=True)
         )
-        generator = btk.draw_blends.CosmosGenerator
-    else:
-        catalog = btk.catalog.CatsimCatalog.from_file(CATALOG_PATH)
-        generator = btk.draw_blends.CatsimGenerator
-
-    catalog.table = Table.from_pandas(
-        catalog.table.to_pandas().sample(frac=1, random_state=0).reset_index(drop=True)
-    )
 
     return catalog, generator, survey
